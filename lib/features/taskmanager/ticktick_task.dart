56d3fc182fbe66fdb2955fbdc0f8821646afe8e5
S M Ishtiak Mahmud Nihal <shakilmahmud.sm10@gmail.com>
2025-11-17 14:53:53 +0600

    Merge branch 'main' of https://github.com/shakilmahmud10/Muslim_Prime

--- lib/features/taskmanager/ticktick_task.dart
+++ lib/features/taskmanager/ticktick_task.dart
@@ -151,6 +151,7 @@ class _TicktickTaskState extends State<TicktickTask>
               tabController: _tabController,
             ),
             gapH12,
+<<<<<<< Updated upstream
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -173,6 +174,33 @@ class _TicktickTaskState extends State<TicktickTask>
                   ],
                 ),
               ],
+=======
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('View Mode'),
+                  Row(
+                    children: [
+                      Text('View'),
+                      SizedBox(width: eightPx),
+                      IconButton(
+                        onPressed: _toggleViewMode, // ভিউ মোড টগল করা
+                        icon: Icon(
+                          _currentViewMode == TaskViewMode.grid
+                              ? Icons
+                                    .view_module // Grid Icon (ছবিতে 4-ডট আইকন)
+                              : Icons
+                                    .view_headline, // List Icon (ছবিতে 3-লাইন আইকন)
+                          color: AppColor.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+>>>>>>> Stashed changes
             ),
             Expanded(
               child: TabBarView(
